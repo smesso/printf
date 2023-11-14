bcbb9ea92a298752b3503e4f6e205572b970666e
siZWITA <mourad.elmohib@gmail.com>
2023-11-14 14:58:49 -0800

    fixing task 0

--- printf.c
+++ printf.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
  * _printf - Custom printf function
@@ -8,46 +6,53 @@
  *
  * Return: Number of characters printed (excluding the null byte)
  */
-int _printf(const char *format, ...) {
-const char *ptr;
-int count = 0;  /* Move the declaration to the beginning */
+int _printf(const char *format, ...)
+{
+	const char *ptr;
+	int count = 0;
 
-va_list args;
-va_start(args, format);
-
-ptr = format;
-
-while (*ptr) {
-if (*ptr == '%') {
-ptr++;
-switch (*ptr) {
-case 'c': {
-int c = va_arg(args, int);
-putchar(c);
-count++;
-break;
-}
-case 's': {
-char *s = va_arg(args, char *);
-while (*s) {
-putchar(*s++);
-count++;
-}
-break;
-}
-case '%': {
-putchar('%');
-count++;
-break;
-}
-}
-} else {
-putchar(*ptr);
-count++;
-}
-ptr++;
-}
-
-va_end(args);
-return (count);
+	va_list args;
+	va_start(args, format);
+	ptr = format;
+	while (*ptr)
+	{
+		if (*ptr == '%')
+		{
+			ptr++;
+			switch (*ptr)
+			{
+				case 'c':
+					{
+						int c = va_arg(args, int);
+						_putchar(c);
+						count++;
+						break;
+					}
+				case 's':
+					{
+						char *s = va_arg(args, char *);
+						while (*s)
+						{
+							_putchar(*s++);
+							count++;
+						}
+						break;
+					}
+				case '%':
+				       {
+						  _putchar('%');
+						  count++;
+						  break;
+					  }
+			}
+		}
+		else
+		{
+			_putchar(*ptr);
+			count++;
+		}
+		ptr++;
+	}
+	va_end(args);
+	return (count);
 }
