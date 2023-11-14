67a4bf8b979cdc5beddb73942c9c815d64efb019
ismail <ismailessoudaigui.com>
2023-11-14 15:15:09 -0800

    updated

--- printf.c
+++ printf.c
@@ -1,58 +1,54 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
-/**
- * _printf - Custom printf function
- * @format: Format string with optional specifiers
- *
- * Return: Number of characters printed (excluding the null byte)
- */
 int _printf(const char *format, ...)
 {
-	const char *ptr;
-	int count = 0;
+const char *ptr;
+int count = 0;
 
-	va_list args;
-	va_start(args, format);
-	ptr = format;
-	while (*ptr)
-	{
-		if (*ptr == '%')
-		{
-			ptr++;
-			switch (*ptr)
-			{
-				case 'c':
-					{
-						int c = va_arg(args, int);
-						_putchar(c);
-						count++;
-						break;
-					}
-				case 's':
-					{
-						char *s = va_arg(args, char *);
-						while (*s)
-						{
-							_putchar(*s++);
-							count++;
-						}
-						break;
-					}
-				case '%':
-				       {
-						  _putchar('%');
-						  count++;
-						  break;
-					  }
-			}
-		}
-		else
-		{
-			_putchar(*ptr);
-			count++;
-		}
-		ptr++;
-	}
-	va_end(args);
-	return (count);
+va_list args;
+va_start(args, format);
+ptr = format;
+while (*ptr)
+{
+if (*ptr == '%')
+{
+ptr++;
+switch (*ptr)
+{
+case 'c':
+{
+int c = va_arg(args, int);
+_putchar(c);
+count++;
+break;
+}
+case 's':
+{
+char *s = va_arg(args, char *);
+while (*s)
+{
+_putchar(*s++);
+count++;
+}
+break;
+}
+case '%':
+{
+_putchar('%');
+count++;
+break;
+}
+}
+}
+else
+{
+_putchar(*ptr);
+count++;
+}
+ptr++;
+}
+va_end(args);
+return (count);
 }
