b01d5b39254d112cc495199b8ec67a14f2dd4292
ismail <ismailessoudaigui.com>
2023-11-14 15:36:27 -0800

    updated

--- printf.c
+++ printf.c
@@ -2,45 +2,46 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...)
-{ const char *ptr;
-int count = 0;
+/**
+ * _printf - Custom printf function
+ * @format: Format string with optional specifiers
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...) {
+const char *ptr;
+int count = 0;  /* Move the declaration to the beginning */
 
 va_list args;
 va_start(args, format);
 ptr = format;
-while (*ptr)
-{
-if (*ptr == '%'){
+while (*ptr) {
+if (*ptr == '%') {
 ptr++;
-switch (*ptr)
-{
-case 'c':
-{
+switch (*ptr) {
+case 'c': {
 int c = va_arg(args, int);
-_putchar(c);
+putchar(c);
 count++;
-break;}
-case 's':
-{
+break;
+}
+case 's': {
 char *s = va_arg(args, char *);
-while (*s)
-{
-_putchar(*s++);
+while (*s) {
+putchar(*s++);
 count++;
 }
 break;
 }
-case '%':
-{
-_putchar('%');
+case '%': {
+putchar('%');
 count++;
-break;}
+break;
 }
 }
-else
+} else 
 {
-_putchar(*ptr);
+putchar(*ptr);
 count++;
 }
 ptr++;
@@ -48,3 +49,4 @@ ptr++;
 va_end(args);
 return (count);
 }
+
